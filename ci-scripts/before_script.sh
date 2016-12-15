6739056a57ca83783d1feec115226967b16d1954
Memory Clutter <memclutter@gmail.com>
2016-12-15 15:42:49 +0300

    fix build error

--- ci-scripts/before_script.sh
+++ ci-scripts/before_script.sh
@@ -5,5 +5,7 @@ composer install
 
 mysql -e 'CREATE DATABASE IF NOT EXISTS test;'
 
+cp tests/app/config/db.mysql.php.dist tests/app/config/db.php
+
 php tests/app/yii migrate --interactive=0
 php tests/app/yii fixture/load * --interactive=0
--- ci-scripts/script.sh
+++ ci-scripts/script.sh
@@ -1,1 +1,3 @@
 #!/usr/bin/env bash
+
+phpunit -c phpunit.xml.dist
