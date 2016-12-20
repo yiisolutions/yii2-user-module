be29c041ba45ce883852a6a647ab5270690f41d0
Memory Clutter <memclutter@gmail.com>
2016-12-20 23:35:53 +0300

    add docblock

--- src/actions/PasswordResetAction.php
+++ src/actions/PasswordResetAction.php
@@ -7,10 +7,36 @@ use yii\base\Action;
 use yii\base\Model;
 use yiisolutions\user\models\PasswordResetFormInterface;
 
+/**
+ * Password reset action implements the password recovery process. For recovery user should be follow a special link.
+ * With a link sends a unique token in query param. If exists token will open a form to enter a new password, after
+ * which the token will be removed.
+ *
+ * Usign:
+ *
+ * ```php
+ * public function actions()
+ * {
+ *     return [
+ *         'password-reset' => [
+ *             'class' => 'yiisolutions\user\actions\PasswordResetAction',
+ *         ],
+ *     ];
+ * }
+ * ```
+ *
+ * @package yiisolutions\user\actions
+ */
 class PasswordResetAction extends Action
 {
+    /**
+     * @var string you can override standard model class and use its
+     */
     public $modelClass = 'yiisolutions\user\models\PasswordResetForm';
 
+    /**
+     * @var string you can override standard view file
+     */
     public $view = '@yiisolutions/user/views/password-reset';
 
     public function run()
